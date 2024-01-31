35e6c004cd5cb27a2f5612781909963635b03729
jrjoy1 <joy2244680@gmail.com>
2024-01-31 18:23:14 +0600

    added for js file

---
+++ for.js
@@ -0,0 +1,14 @@
+let num = 0;  //loop variable
+while (num < 5) {  //loop condition
+    console.log(num);  //loop statement
+    num++;  //loop variable change
+}
+
+/**
+ * 
+ * 
+ * for( first part ; sevond part ; third part){
+    loop statement
+}
+ * 
+ */
