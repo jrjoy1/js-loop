6a517de36ea5fca714a98f8b14f3f98e70140853
jrjoy1 <joy2244680@gmail.com>
2024-01-31 17:45:30 +0600

    added loop files

---
+++ while.js
@@ -0,0 +1,13 @@
+const number = 7;
+if (number < 10) {
+    console.log('coto ekta number paisi');
+}
+console.log('something new is cooking');
+
+//------------while loop_________________
+let num = 0;
+while (num < 5) {
+    console.log('looping', num);
+    // num = num + 1;
+    num += 1;
+}
